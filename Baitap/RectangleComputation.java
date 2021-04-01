1f214f49152e9c8ae7e2daef6148f9718124cad3
Quoc Viet <duongquocv@gmail.com>
2021-04-01 23:36:16 +0700

    Task01

---
+++ Baitap/RectangleComputation.java
@@ -0,0 +1,19 @@
+package Baitap;
+
+public class RectangleComputation {
+    public static void main(String[] args) {
+        double length=3.2222;
+        double width = 1.23576426;
+        double area, perimeter;
+        area = length * width;
+        perimeter = (length + width) * 2;
+        System.out.print("The length is ");
+        System.out.println(length);
+        System.out.print("The width is ");
+        System.out.println(width);
+        System.out.print("The area is ");
+        System.out.println(area);
+        System.out.print("The perimeter is ");
+        System.out.println(perimeter);
+    }
+}
